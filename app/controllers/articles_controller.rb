408014d8872af9073ea001c3db08b56c20bff064
Giorgi Meladze <giorgi.meladze711@gmail.com>
2020-01-28 19:47:05 +0400

    update article controller user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     # @article.user_id = User.first.id
     # @article.save
     if @article.save
